12b071a37719f4b882a6a5b2967c2555a6ccdcb1
vo_o <vo_o@etna-alternance.net>
2024-03-08 08:06:19 +0100

    [ADD] => adding the loading page

--- freeGG/App.tsx
+++ freeGG/App.tsx
@@ -1,22 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet, Dimensions, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { TabNavigator } from "./components/organisms";
 import { GameContext } from "./context";
 import { Game } from "./interfaces";
 import { fetchSortedData } from "./utils";
 import { StatusBar } from "expo-status-bar";
+import LottieView from "lottie-react-native";
 
 export default function App() {
   const [allGames, setAllGames] = useState<Game[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const animation = useRef(null) as any;
 
   useEffect(() => {
     fetchSortedData("popularity").then((data) => {
       setAllGames(data);
+      setIsLoading(false);
     });
   }, []);
 
-  return (
+  return isLoading ? (
+    <View style={styles.animationContainer}>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: Dimensions.get("window").width,
+          height: Dimensions.get("window").height / 4,
+          backgroundColor: "#000",
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require("./assets/blueLoading.json")}
+      />
+    </View>
+  ) : (
     <GameContext.Provider
       value={{ gamesState: allGames, setGamesState: setAllGames }}
     >
@@ -27,3 +45,15 @@ export default function App() {
     </GameContext.Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+});
